4745b9248287d87e6f22a9029dbd7a368d8e1222
Omer <elomer@gmail.com>
2017-07-01 15:46:59 +0300

    declare test uint functions

---
+++ CardsVsHumanity/app/src/test/java/com/omerbarr/cardsvshumanity/UnitTest.java
@@ -0,0 +1,40 @@
+package com.omerbarr.cardsvshumanity;
+
+import com.omerbarr.cardsvshumanity.Utils.PlayerListArrayAdapter;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+
+
+    // Test that game can be playing with min 2 players and max 5 players
+    @Test
+    public void minMaxPlayers() throws Exception {
+        // Objects
+
+    }
+
+    // Test that only if device has the right secure code he can connects to the server
+
+    // Test that the name and code has to be at least 3 letters
+
+    // Test that player manager only continue the game chronological move after he got respond from all players
+
+    // Test that When a player wins the table score is updated
+
+    // Test that when a player get black cards with few empty answers, he can pick the same amount of answers
+
+}
